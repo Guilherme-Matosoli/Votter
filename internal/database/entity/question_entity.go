2b8ab0529cc342caffd26667c8442e969015435d
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-26 20:13:19 -0300

    add json identifier in question entity :fire:

--- internal/database/entity/question_entity.go
+++ internal/database/entity/question_entity.go
@@ -1,8 +1,7 @@
 package entity
 
 type question struct {
-	Id          string
-	Poll_id     string
-	Description string
-	Votes       int
+	Id          string `json:"id"`
+	Poll_id     string `json:"poll_id"`
+	Description string `json:"description"`
 }
