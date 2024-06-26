e1d26b097559133d0dd1b8034dbc4d0791292212
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-26 20:16:54 -0300

    add newQuestion func in question entity :robot:

--- internal/database/entity/question_entity.go
+++ internal/database/entity/question_entity.go
@@ -1,7 +1,17 @@
 package entity
 
+import "github.com/google/uuid"
+
 type question struct {
 	Id          string `json:"id"`
 	Poll_id     string `json:"poll_id"`
 	Description string `json:"description"`
+}
+
+func NewQuestion(poll_id string, description string) *question {
+	return &question{
+		Id:          uuid.New().String(),
+		Poll_id:     poll_id,
+		Description: description,
+	}
 }
