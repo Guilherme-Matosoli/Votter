74cc3776fa1905c378722bf234d3ff7cbea97376
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-26 20:02:08 -0300

    remove description field from poll_entity :heavy_minus_sign

--- internal/database/entity/poll_entity.go
+++ internal/database/entity/poll_entity.go
@@ -4,17 +4,15 @@ import "github.com/google/uuid"
 
 type poll struct {
 	Id          string `json:"id"`
-	Description string `json:"description"`
 	Title       string `json:"title"`
-	Created_at  string `json:"created_at`
+	Created_at  string `json:"created_at"`
 	Total_votes int    `json:"total_votes"`
 }
 
 func CreatePoll(description string, title string, created_at string) *poll {
 	return &poll{
-		Id:          uuid.New().String(),
-		Description: description,
-		Title:       title,
-		Created_at:  created_at,
+		Id:         uuid.New().String(),
+		Title:      title,
+		Created_at: created_at,
 	}
 }
