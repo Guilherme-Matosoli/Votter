bf68b9034e0465c01cc15f31a7816a5319174d9a
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-26 14:38:53 -0300

    configure migration :rocket:

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -1,12 +1,46 @@
 package database
 
-import "github.com/Guilherme-Matosoli/votter/internal/database"
+import "fmt"
 
 func RunMigration() {
-	conn, err := database.Connection()
+	conn, err := Connection()
 	if err != nil {
 		panic(err)
 	}
 
-	sql := `CREATE TABLE `
+	defer conn.Close()
+
+	querys := []string{
+		`CREATE TABLE IF NOT EXISTS polls (
+			id VARCHAR PRIMARY KEY,
+			title VARCHAR (255) NOT NULL,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			total_votes INT DEFAULT 0
+		)`,
+
+		`CREATE TABLE IF NOT EXISTS questions (
+  		id VARCHAR PRIMARY KEY,
+  		poll_id VARCHAR REFERENCES polls(id) ON DELETE CASCADE,
+  		description TEXT NOT NULL,
+  		votes INT DEFAULT 0
+		)`,
+
+		`CREATE TABLE IF NOT EXISTS ips (
+			id VARCHAR PRIMARY KEY,
+			ip_address VARCHAR NOT NULL,
+			voted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			poll_id VARCHAR REFERENCES polls(id) ON DELETE CASCADE
+		)
+		`,
+	}
+
+	for _, query := range querys {
+		_, err = conn.Query(query)
+
+		if err != nil {
+			fmt.Println("Error has happen: ", err)
+		}
+	}
+
+	fmt.Println("Migration execution success!")
 }
