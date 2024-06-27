bbcbcb7a6cb868bce2b35d8b0a26e626385629cc
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 18:23:36 -0300

    change question struct from private to public :rocket:

--- internal/database/entity/question_entity.go
+++ internal/database/entity/question_entity.go
@@ -2,15 +2,15 @@ package entity
 
 import "github.com/google/uuid"
 
-type question struct {
+type Question struct {
 	Id          string `json:"id"`
 	Poll_id     string `json:"poll_id"`
 	Description string `json:"description"`
 	Title       string `json:"title"`
 }
 
-func NewQuestion(poll_id string, description string, title string) *question {
-	return &question{
+func NewQuestion(poll_id string, description string, title string) *Question {
+	return &Question{
 		Id:          uuid.New().String(),
 		Poll_id:     poll_id,
 		Description: description,
