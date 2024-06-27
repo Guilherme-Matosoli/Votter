9727a972d027429b12cba73d12b8fac7deed76f4
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 16:29:53 -0300

    add title field again :fire:

--- internal/database/entity/question_entity.go
+++ internal/database/entity/question_entity.go
@@ -6,12 +6,14 @@ type question struct {
 	Id          string `json:"id"`
 	Poll_id     string `json:"poll_id"`
 	Description string `json:"description"`
+	Title       string `json:"title"`
 }
 
-func NewQuestion(poll_id string, description string) *question {
+func NewQuestion(poll_id string, description string, title string) *question {
 	return &question{
 		Id:          uuid.New().String(),
 		Poll_id:     poll_id,
 		Description: description,
+		Title:       title,
 	}
 }
