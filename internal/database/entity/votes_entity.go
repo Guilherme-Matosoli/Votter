0f9be0a530565e74953b0dfd16b242a25c023945
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-26 21:41:56 -0300

    add NewVote function to votes entity :tada:

--- internal/database/entity/votes_entity.go
+++ internal/database/entity/votes_entity.go
@@ -1,8 +1,19 @@
 package entity
 
+import "github.com/google/uuid"
+
 type vote struct {
 	Id         string
 	Ip_address string
 	Voted_at   string
 	Poll_id    string
+}
+
+func NewVote(ip_address string, poll_id string) *vote {
+	return &vote{
+		Id:         uuid.New().String(),
+		Ip_address: ip_address,
+		Voted_at:   "",
+		Poll_id:    poll_id,
+	}
 }
