0a7a8e85237b148eca85fb3ce5686e79c99d1217
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 16:13:09 -0300

    remove total_votes from polls entity :fire:

--- internal/database/entity/poll_entity.go
+++ internal/database/entity/poll_entity.go
@@ -1,18 +1,23 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/Guilherme-Matosoli/votter/internal/utils"
+	"github.com/google/uuid"
+)
 
 type poll struct {
-	Id          string `json:"id"`
-	Title       string `json:"title"`
-	Created_at  string `json:"created_at"`
-	Total_votes int    `json:"total_votes"`
+	Id          string    `json:"id"`
+	Title       string    `json:"title"`
+	Created_at  time.Time `json:"created_at"`
+	Total_votes int       `json:"total_votes"`
 }
 
-func NewPoll(description string, title string, created_at string) *poll {
+func NewPoll(title string) *poll {
 	return &poll{
 		Id:         uuid.New().String(),
 		Title:      title,
-		Created_at: created_at,
+		Created_at: utils.GetTime(),
 	}
 }
