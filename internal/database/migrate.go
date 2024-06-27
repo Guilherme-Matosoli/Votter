7c26e92ec04bea72fc5be0c9cc5a4d904efbe62f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 16:28:14 -0300

    add title column on question table :tada:

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -20,7 +20,8 @@ func RunMigration() {
 		`CREATE TABLE IF NOT EXISTS questions (
   		id VARCHAR PRIMARY KEY,
   		poll_id VARCHAR REFERENCES polls(id) ON DELETE CASCADE,
-  		description TEXT NOT NULL
+  		description TEXT NOT NULL,
+			title TEXT NOT NULL
 		)`,
 
 		`CREATE TABLE IF NOT EXISTS votes (
