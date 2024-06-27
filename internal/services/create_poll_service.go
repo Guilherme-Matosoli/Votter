36cb1d7cfb08bd4aa17bea16f6646d9227a85c10
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 17:06:44 -0300

    change param type to a pointer :robot:

--- internal/services/create_poll_service.go
+++ internal/services/create_poll_service.go
@@ -11,7 +11,7 @@ type pollPropertys struct {
 	Title string `json:"title"`
 }
 
-func CreatePoll(db *sql.DB, poll pollPropertys) (string, error) {
+func CreatePoll(db *sql.DB, poll *pollPropertys) (string, error) {
 	newPoll := entity.NewPoll(poll.Title)
 
 	_, err := db.Exec("INSERT INTO polls (id, title, created_at) VALUES ($1,$2,$3)", newPoll.Id, newPoll.Title, newPoll.Created_at)
