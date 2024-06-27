004891b9dd96fccb8fffd7f19f5fe9f543af4b24
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 17:06:11 -0300

    finish create_question_service :tada:

--- internal/services/create_question_service.go
+++ internal/services/create_question_service.go
@@ -1,1 +1,28 @@
 package services
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
+)
+
+type questionPropertys struct {
+	Poll_id     string
+	Description string
+	Title       string
+}
+
+func CreateQuestion(db *sql.DB, props *questionPropertys) (string, error) {
+	newQuestion := entity.NewQuestion(props.Poll_id, props.Description, props.Title)
+
+	_, err := db.Exec("INSERT INTO questions (id, poll_id, title, description, title) VALUES ($1,$2,$3, $4)",
+		newQuestion.Id, newQuestion.Poll_id, newQuestion.Description, newQuestion.Title,
+	)
+
+	if err != nil {
+		fmt.Println("Error happens on create_question_service: ", err)
+	}
+
+	return "Question created", err
+}
