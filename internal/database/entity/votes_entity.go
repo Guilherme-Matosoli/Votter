2711dc41121c61e144bbec4e0fac261e6a65c0b6
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-27 18:24:30 -0300

    change vote struct from private to public :rocket:

--- internal/database/entity/votes_entity.go
+++ internal/database/entity/votes_entity.go
@@ -7,15 +7,15 @@ import (
 	"github.com/google/uuid"
 )
 
-type vote struct {
+type Vote struct {
 	Id         string
 	Ip_address string
 	Voted_at   time.Time
 	Poll_id    string
 }
 
-func NewVote(ip_address string, poll_id string) *vote {
-	return &vote{
+func NewVote(ip_address string, poll_id string) *Vote {
+	return &Vote{
 		Id:         uuid.New().String(),
 		Ip_address: ip_address,
 		Voted_at:   utils.GetTime(),
