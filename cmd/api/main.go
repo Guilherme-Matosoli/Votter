64a6cd313aeb162ce4e3ce69063cabb9f3e146a4
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 15:19:33 -0300

    configure chi router :fire:

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Guilherme-Matosoli/votter/internal/database"
+import (
+	"net/http"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database"
+	"github.com/go-chi/chi"
+)
 
 func main() {
 	database.RunMigration()
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world"))
+	})
+
+	http.ListenAndServe(":4000", r)
 }
