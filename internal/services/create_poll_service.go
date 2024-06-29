2e0ce06f664316a770fba63fea81c70cca4ac333
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 17:03:35 -0300

    remove pointer from question list :fire:

--- internal/services/create_poll_service.go
+++ internal/services/create_poll_service.go
@@ -7,7 +7,7 @@ import (
 	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
 )
 
-func CreatePoll(db *sql.DB, poll *entity.Poll, questions []*entity.Question) (string, error) {
+func CreatePoll(db *sql.DB, poll *entity.Poll, questions []entity.Question) (string, error) {
 	newPoll := entity.NewPoll(poll.Title)
 
 	_, err := db.Exec("INSERT INTO polls (id, title, created_at) VALUES ($1,$2,$3)", newPoll.Id, newPoll.Title, newPoll.Created_at)
