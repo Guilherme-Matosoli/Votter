46fad3234f73a40862f3260137c4e9cb145a6b5c
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 17:13:29 -0300

    fix create_question query that contains title twice :bug:

--- internal/services/create_question_service.go
+++ internal/services/create_question_service.go
@@ -10,7 +10,7 @@ import (
 func CreateQuestion(db *sql.DB, props *entity.Question) (string, error) {
 	newQuestion := entity.NewQuestion(props.Poll_id, props.Description, props.Title)
 
-	_, err := db.Exec("INSERT INTO questions (id, poll_id, title, description, title) VALUES ($1,$2,$3, $4)",
+	_, err := db.Exec("INSERT INTO questions (id, poll_id, description, title) VALUES ($1,$2,$3, $4)",
 		newQuestion.Id, newQuestion.Poll_id, newQuestion.Description, newQuestion.Title,
 	)
 
