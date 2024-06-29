8312d7fce531a7ebbcf6b422b53fbe7ca390cdbb
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 16:30:32 -0300

    handle with data in crate_poll_controller :fire:

--- internal/controllers/create_poll_controller.go
+++ internal/controllers/create_poll_controller.go
@@ -1,7 +1,35 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database"
+	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
+	//"github.com/Guilherme-Matosoli/votter/internal/services"
+)
+
+type requestBody struct {
+	Info      entity.Poll       `json:"info"`
+	Questions []entity.Question `json:"questions"`
+}
 
 func CreatePollController(w http.ResponseWriter, r *http.Request) {
+	_, error := database.Connection()
+	if error != nil {
+		fmt.Println("error happen: ", error)
+		panic(error)
+	}
+
+	var input requestBody
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		fmt.Println("error has happen: ", err)
+	}
+
+	fmt.Println("infos: ", input.Info)
+	fmt.Println("questions: ", input.Questions)
 
 }
