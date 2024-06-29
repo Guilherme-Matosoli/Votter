0f74a6a20f6e3847a79b1173399d3fd4fb0b623a
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-28 21:38:09 -0300

    validate if user has vote in the last 24h :fire:

--- internal/services/create_vote_service.go
+++ internal/services/create_vote_service.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 
 	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
+	"github.com/Guilherme-Matosoli/votter/internal/utils"
 )
 
-func CreateVote(db *sql.DB, props *entity.Vote) error {
+func CreateVote(db *sql.DB, props *entity.Vote) (string, error) {
 	var lastVote entity.Vote
 	row, err := db.Query(`SELECT "voted_at" FROM votes WHERE "id" = $1`, props.Ip_address)
 	if err != nil {
 		fmt.Println("Error happens: ", err)
 	}
 	row.Scan(&lastVote)
+
+	validTimeToVote := utils.ValidateTime(*&lastVote.Voted_at)
+
+	if !validTimeToVote {
+		return "Ip already vote in the last 24h", err
+	}
 
 	newVote := entity.NewVote(props.Ip_address, props.Poll_id)
 
@@ -24,5 +31,5 @@ func CreateVote(db *sql.DB, props *entity.Vote) error {
 		fmt.Println("Error has happen on create_vote_service: ", err)
 	}
 
-	return err
+	return "Success", err
 }
