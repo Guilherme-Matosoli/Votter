3e3673522ba050c5fe059f3d3ec89fb92e47f221
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 22:28:59 -0300

    insert voted in on votes entity :fire:

--- internal/database/entity/votes_entity.go
+++ internal/database/entity/votes_entity.go
@@ -11,14 +11,16 @@ type Vote struct {
 	Id         string
 	Ip_address string
 	Voted_at   time.Time
+	Voted_in   string
 	Poll_id    string
 }
 
-func NewVote(ip_address string, poll_id string) *Vote {
+func NewVote(ip_address string, poll_id string, voted_in string) *Vote {
 	return &Vote{
 		Id:         uuid.New().String(),
 		Ip_address: ip_address,
 		Voted_at:   utils.GetTime(),
+		Voted_in:   voted_in,
 		Poll_id:    poll_id,
 	}
 }
