6f411a1adafc4fe70c0b7753fb6e0ce1ef439fbd
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-06-29 22:38:39 -0300

    handle with data in create_vote_controller :fire:

--- internal/controllers/create_vote_controller.go
+++ internal/controllers/create_vote_controller.go
@@ -1,7 +1,24 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
+	"github.com/Guilherme-Matosoli/votter/internal/utils"
+)
+
+type createVoteRequestBody struct {
+	Ip_address string `json:"ip_address"`
+	Poll_id    string `json:"poll_id"`
+	Voted_in   string `json:"voted_in"`
+}
 
 func CreateVoteController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(r.RemoteAddr))
+	ip := utils.GetIp(r)
+	input := entity.Vote{Ip_address: ip}
+	json.NewDecoder(r.Body).Decode(&input)
+
+	fmt.Println(input)
 }
