75445bff47257208b1c46e990705711da652c680
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-01 17:15:02 -0300

    verify if year is 1 (default year to type time.Time) :tada:

--- internal/utils/validate_time.go
+++ internal/utils/validate_time.go
@@ -8,6 +8,10 @@ func ValidateTime(lastTime time.Time) bool {
 	timeSubtraction := lastTime.Sub(GetTime())
 	maxTime := 24 * time.Hour
 
+	if lastTime.Year() == 1 {
+		return true
+	}
+
 	if timeSubtraction < maxTime {
 		return false
 	}
