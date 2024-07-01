991b7ed98f8e6ce38b2bbd26470ac608acb6c918
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-01 19:02:47 -0300

    implements voted_in field in create_vote query :boom:

--- internal/services/create_vote_service.go
+++ internal/services/create_vote_service.go
@@ -10,13 +10,12 @@ import (
 
 func CreateVote(db *sql.DB, props *entity.Vote) (string, error) {
 	var lastVote entity.Vote
-	row, err := db.Query(`SELECT "voted_at" FROM votes WHERE "id" = $1`, props.Ip_address)
+	err := db.QueryRow(`SELECT * FROM votes WHERE "ip_address" = $1`, props.Ip_address).Scan(&lastVote.Id, &lastVote.Ip_address, &lastVote.Voted_at, &lastVote.Voted_in, &lastVote.Poll_id)
 	if err != nil {
 		fmt.Println("Error happens: ", err)
 	}
-	row.Scan(&lastVote)
 
-	validTimeToVote := utils.ValidateTime(*&lastVote.Voted_at)
+	validTimeToVote := utils.ValidateTime(lastVote.Voted_at)
 
 	if !validTimeToVote {
 		return "Ip already vote in the last 24h", err
@@ -24,8 +23,8 @@ func CreateVote(db *sql.DB, props *entity.Vote) (string, error) {
 
 	newVote := entity.NewVote(props.Ip_address, props.Poll_id, props.Voted_in)
 
-	_, error := db.Exec("INSERT INTO votes (id, ip_address, voted_at, poll_id) VALUES ($1, $2, $3, $4)",
-		newVote.Id, newVote.Ip_address, newVote.Voted_at, newVote.Poll_id)
+	_, error := db.Exec("INSERT INTO votes (id, ip_address, voted_at, voted_in,poll_id) VALUES ($1, $2, $3, $4, $5)",
+		newVote.Id, newVote.Ip_address, newVote.Voted_at, newVote.Voted_in, newVote.Poll_id)
 
 	if error != nil {
 		fmt.Println("Error has happen on create_vote_service: ", err)
