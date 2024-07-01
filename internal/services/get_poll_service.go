0e0140912d2df15b1ac76ff750e711fb2d43149b
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-01 15:39:41 -0300

    handle get poll questions :fire:

--- internal/services/get_poll_service.go
+++ internal/services/get_poll_service.go
@@ -8,12 +8,24 @@ import (
 )
 
 func GetPoll(db *sql.DB, poll_id string) (string, error) {
-	var questionsList []entity.Question
+	var questionsList []*entity.Question
 
 	questions, err := db.Query(`SELECT * FROM questions WHERE poll_id = $1`, poll_id)
 	if err != nil {
 		fmt.Println("Error happens in get_poll_service: ", err)
 		return "Error", err
+	}
+
+	for questions.Next() {
+		var question entity.Question
+
+		err := questions.Scan(&question.Id, &question.Title, &question.Description)
+		if err != nil {
+			fmt.Println("Error happen in get_poll_service: ", err)
+			return "Error", err
+		}
+
+		questionsList = append(questionsList, &question)
 	}
 
 	return "", nil
