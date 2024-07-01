82323ec700b0baaeb9a3263146104ed4392ba33f
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-01 15:45:51 -0300

    remove logic to get quesitons from get_poll_service :heavy_minus_sign:

--- internal/services/get_poll_service.go
+++ internal/services/get_poll_service.go
@@ -2,31 +2,8 @@ package services
 
 import (
 	"database/sql"
-	"fmt"
-
-	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
 )
 
 func GetPoll(db *sql.DB, poll_id string) (string, error) {
-	var questionsList []*entity.Question
-
-	questions, err := db.Query(`SELECT * FROM questions WHERE poll_id = $1`, poll_id)
-	if err != nil {
-		fmt.Println("Error happens in get_poll_service: ", err)
-		return "Error", err
-	}
-
-	for questions.Next() {
-		var question entity.Question
-
-		err := questions.Scan(&question.Id, &question.Title, &question.Description)
-		if err != nil {
-			fmt.Println("Error happen in get_poll_service: ", err)
-			return "Error", err
-		}
-
-		questionsList = append(questionsList, &question)
-	}
-
 	return "", nil
 }
