d035af1c530c61c0eef65449d62db99c7c4cf6a9
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-01 15:48:29 -0300

    handle get question informations :rocket:

--- internal/services/get_poll_service.go
+++ internal/services/get_poll_service.go
@@ -2,8 +2,21 @@ package services
 
 import (
 	"database/sql"
+	"fmt"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
 )
 
 func GetPoll(db *sql.DB, poll_id string) (string, error) {
+	var poll *entity.Poll
+
+	row, err := db.Query(`SELECT * FROM polls WHERE id = $1`, poll_id)
+	if err != nil {
+		fmt.Println("Error happens on get_poll_service: ", err)
+		return "Error", err
+	}
+
+	row.Scan(&poll)
+
 	return "", nil
 }
