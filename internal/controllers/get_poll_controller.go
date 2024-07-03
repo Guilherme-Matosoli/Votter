b3bd2f9a3c50bba10dd5217cf3c47cd296191231
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-03 15:44:09 -0300

    connect with db and return error if it doesn't work :boom:

--- internal/controllers/get_poll_controller.go
+++ internal/controllers/get_poll_controller.go
@@ -1,7 +1,22 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Guilherme-Matosoli/votter/internal/database"
+)
 
 func GetPoll(w http.ResponseWriter, r *http.Request) {
+	conn, err := database.Connection()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(&response{Message: "Internal Server Error"})
+
+		fmt.Println("Error happens in create_vote_controller: ", err)
+		return
+	}
+	defer conn.Close()
 
 }
