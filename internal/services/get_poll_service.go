d13361834669741941affd2120004cfee32ce436
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-04 14:43:42 -0300

    change Query from QueryRow cause Query bring a rows 'array' :wrench:

--- internal/services/get_poll_service.go
+++ internal/services/get_poll_service.go
@@ -3,47 +3,33 @@ package services
 import (
 	"database/sql"
 	"fmt"
-
-	"github.com/Guilherme-Matosoli/votter/internal/database/entity"
 )
 
 type poll struct {
-	*entity.Poll
+	Id          string      `json:"id"`
+	Title       string      `json:"title"`
 	Total_Votes int         `json:"total_votes"`
 	Questions   []*question `json:"questions"`
 }
 
 func GetPoll(db *sql.DB, poll_id string) (*poll, error) {
-	poll := &poll{Poll: &entity.Poll{}}
+	poll := &poll{}
 
 	questions, err := GetQuestion(db, poll_id)
 	if err != nil {
 		return poll, err
 	}
 
-	vas := *questions[0]
-
-	fmt.Println("questions: ", vas)
-
-	fmt.Println("Pass from getQuestion")
-
 	poll.Questions = questions
 
-	for range questions {
-		poll.Total_Votes += 1
+	for _, value := range questions {
+		poll.Total_Votes += value.Votes
 	}
 
-	row, err := db.Query(`SELECT id, title, created_at FROM polls WHERE id = $1`, poll_id)
-	if err != nil {
+	error := db.QueryRow(`SELECT id, title FROM polls WHERE id = $1`, poll_id).Scan(&poll.Id, &poll.Title)
+	if error != nil {
 		fmt.Println("Error happens on get_poll_service: ", err)
-		return poll, err
-	}
-
-	row.Scan(&poll.Id, &poll.Title, &poll.Created_at)
-
-	if err = row.Err(); err != nil {
-		fmt.Println("Error on row: ", err)
-		return poll, err
+		return poll, error
 	}
 
 	return poll, nil
