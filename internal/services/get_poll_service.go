04c96d24e66375892ad24506de3353af842a3312
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-04 14:15:35 -0300

    fix query :hammer:

--- internal/services/get_poll_service.go
+++ internal/services/get_poll_service.go
@@ -9,37 +9,42 @@ import (
 
 type poll struct {
 	*entity.Poll
-	Total_Votes int `json:"total_votes"`
+	Total_Votes int         `json:"total_votes"`
+	Questions   []*question `json:"questions"`
 }
 
-type pollInfos struct {
-	Poll      poll        `json:"poll"`
-	Questions []*question `json:"questions"`
-}
-
-func GetPoll(db *sql.DB, poll_id string) error {
-	var poll *pollInfos
+func GetPoll(db *sql.DB, poll_id string) (*poll, error) {
+	poll := &poll{Poll: &entity.Poll{}}
 
 	questions, err := GetQuestion(db, poll_id)
 	if err != nil {
-		return err
+		return poll, err
 	}
+
+	vas := *questions[0]
+
+	fmt.Println("questions: ", vas)
 
 	fmt.Println("Pass from getQuestion")
 
 	poll.Questions = questions
 
 	for range questions {
-		poll.Poll.Total_Votes += 1
+		poll.Total_Votes += 1
 	}
 
-	row, err := db.Query(`SELECT * FROM polls WHERE id = $1`, poll_id)
+	row, err := db.Query(`SELECT id, title, created_at FROM polls WHERE id = $1`, poll_id)
 	if err != nil {
 		fmt.Println("Error happens on get_poll_service: ", err)
-		return err
+		return poll, err
 	}
 
-	row.Scan(&poll)
+	row.Scan(&poll.Id, &poll.Title, &poll.Created_at)
 
-	return nil
+	if err = row.Err(); err != nil {
+		fmt.Println("Error on row: ", err)
+		return poll, err
+	}
+
+	return poll, nil
 }
