e5538bd3727e44059f763575724fbe0bb8d41b03
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-25 13:28:42 -0300

    :recycle: refactor(database/entity): Refactor `Question` entity to replace `description` and `title` with `option`

--- internal/database/entity/question_entity.go
+++ internal/database/entity/question_entity.go
@@ -3,17 +3,15 @@ package entity
 import "github.com/google/uuid"
 
 type Question struct {
-	Id          string `json:"id"`
-	Poll_id     string `json:"poll_id"`
-	Description string `json:"description"`
-	Title       string `json:"title"`
+	Id      string `json:"id"`
+	Poll_id string `json:"poll_id"`
+	Option  string `json:"option"`
 }
 
-func NewQuestion(poll_id string, description string, title string) *Question {
+func NewQuestion(poll_id string, option string) *Question {
 	return &Question{
-		Id:          uuid.New().String(),
-		Poll_id:     poll_id,
-		Description: description,
-		Title:       title,
+		Id:      uuid.New().String(),
+		Poll_id: poll_id,
+		Option:  option,
 	}
 }
