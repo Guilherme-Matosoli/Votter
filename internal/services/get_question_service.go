6ae05e3e2730810b882029b25fe0ddc8207bb402
Guilherme Matos <guilhermematosgto2015@outlook.com>
2024-07-26 16:46:17 -0300

    :recycle: refactor(services): Refactor `GetQuestion` to replace `title` and `description` with `option`

--- internal/services/get_question_service.go
+++ internal/services/get_question_service.go
@@ -6,16 +6,15 @@ import (
 )
 
 type question struct {
-	Id          string `json:"id"`
-	Description string `json:"description"`
-	Title       string `json:"title"`
-	Votes       int    `json:"votes"`
+	Id     string `json:"id"`
+	Option string `json:"option"`
+	Votes  int    `json:"votes"`
 }
 
 func GetQuestion(db *sql.DB, poll_id string) ([]*question, error) {
 	var questionsList []*question
 
-	query := `SELECT questions.id, questions.title, questions.description, COUNT(votes.id) as votes
+	query := `SELECT questions.id, questions.option, COUNT(votes.id) as votes
 	 					FROM questions
 						LEFT JOIN votes
 						ON votes.voted_in = questions.Id
@@ -31,7 +30,7 @@ func GetQuestion(db *sql.DB, poll_id string) ([]*question, error) {
 	for questions.Next() {
 		question := &question{}
 
-		err := questions.Scan(&question.Id, &question.Title, &question.Description, &question.Votes)
+		err := questions.Scan(&question.Id, &question.Option, &question.Votes)
 		if err != nil {
 			fmt.Println("Error happens in get_question_service (loop quesitons) ", err)
 		}
@@ -41,7 +40,7 @@ func GetQuestion(db *sql.DB, poll_id string) ([]*question, error) {
 
 	if err = questions.Err(); err != nil {
 		fmt.Println("erro happens: ", err)
-		return nil, err
+		return nil, nil
 	}
 
 	return questionsList, nil
